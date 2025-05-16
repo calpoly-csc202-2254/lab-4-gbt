609a7b528c444c1a5b5201cd6c6349f614ff4fa3
qp1d0 <gurparsadkk@gmail.com>
2025-05-16 15:42:24 -0700

    Purpose statements / headers

--- bst.py
+++ bst.py
@@ -7,3 +7,29 @@ sys.setrecursionlimit(10**6)
 
 def example_fun(x : int) -> bool:
     return x < 142
+
+BinTree : TypeAlias = Union["Node" : Any, None]
+
+@dataclass (frozen=True)
+class BinarySearchTree:
+    def comes_before(listy):
+        listy = []
+        return listy - 1
+
+    # Given a BinarySearchTree, return True if tree is empty, False otherwise
+    def is_empty(self) -> bool:
+        e = len(listy)
+        match e:
+            case 0:
+                return True
+            case :
+                return False
+
+    # Adds value to tree by using comes_before function to determine which path to take @ each node, inserts into left subtree if value comese before value stores, right subtree otherwise
+    def insert():
+
+    # Returns True if value is stored in tree and False if else
+    def lookup():
+
+    # Removes value from tree (if there) while preserving binary search tree property for given node's value, if many nodes containing values need to be removed, only one such node will be removed
+    def delete():
