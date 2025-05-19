0d7b842fbe6c6ed063f248257d46e3ebed94186e
tphan56 <tphan56@calpoly.edu>
2025-05-19 15:09:28 -0700

    functions

--- bst.py
+++ bst.py
@@ -1,6 +1,7 @@
+from ast import Call
 import sys
 import unittest
-from typing import *
+from typing import * 
 from dataclasses import dataclass
 sys.setrecursionlimit(10**6)
 
@@ -8,28 +9,66 @@ sys.setrecursionlimit(10**6)
 def example_fun(x : int) -> bool:
     return x < 142
 
-BinTree : TypeAlias = Union["Node" : Any, None]
+BinTree : TypeAlias = Union["BinarySearchTree", None]
+
+        
+#determines if one value comes before another        
+def comes_before(a, b) -> bool: #boolean
+    if a > b:
+        return True
+    else:
+        return False
+    
 
 @dataclass (frozen=True)
 class BinarySearchTree:
-    def comes_before(listy):
-        listy = []
-        return listy - 1
+    value: Any
+    comes_before: Callable[[Any, Any], bool] #takes two type Anys, returns bool
+    left: BinTree
+    right: BinTree
 #a
-    # Given a BinarySearchTree, return True if tree is empty, False otherwise
-    def is_empty(self) -> bool:
-        e = len(listy)
-        match e:
-            case 0:
-                return True
-            case :
-                return False
+# Given a BinarySearchTree, return True if tree is empty, False otherwise
+def is_empty(l: BinTree) -> bool:
+    match l:
+        case None:
+            return True
+        case BinarySearchTree(v, _, left, right):
+            return False
 
-    # Adds value to tree by using comes_before function to determine which path to take @ each node, inserts into left subtree if value comese before value stores, right subtree otherwise
-    def insert():
+    # Adds value to tree by using comes_before function to determine which path to take @ each node, 
+    # inserts into left subtree if value comese before value stores, right subtree otherwise
+   
+def helper(i: BinTree, new_value: Any, comes_before) -> BinTree:
+    match i:
+        case None:
+            return BinarySearchTree(new_value, comes_before, None, None)
+        case BinarySearchTree(value, comes_before, left, right):
+            if comes_before(new_value, value) is True:
+                return BinarySearchTree(value, comes_before, helper(left, new_value, comes_before), right)
+            else:
+                return BinarySearchTree(value, comes_before, left, helper(right, new_value, comes_before))
 
+def insert(l: BinTree, val: Any, comes_before) -> BinTree:
+    return helper(l, val, comes_before)
+    
     # Returns True if value is stored in tree and False if else
-    def lookup():
+    #Instead, you will use the comes_before function to determine if the value appears in the tree. 
+    # More specifically, when comparing two values, if neither value "comes before" the other, 
+    # then the values will be considered equal (i.e., for our purposes, (not (a < b) and not (b < a)) -> a = b).
 
-    # Removes value from tree (if there) while preserving binary search tree property for given node's value, if many nodes containing values need to be removed, only one such node will be removed
+    def lookup(i: BinTree, look: Any, comes_before) -> bool:
+       match i:
+           case None:
+               return False
+           case BinarySearchTree(value, comes_before, left, right):
+               if comes_before(left, value) and comes_before(right, value) is True:
+                   return True
+               else: 
+                   return False
+                   
+
+    # Removes value from tree (if there) while preserving binary search tree property for given node's value, 
+    # if many nodes containing values need to be removed, only one such node will be removed
     def delete():
+        pass
+
--- bst_tests.py
+++ bst_tests.py
@@ -13,21 +13,21 @@ class BSTTests(unittest.TestCase):
     
     def test_is_empty(self):
         self.assertEqual(True, is_empty(None))
-        self.assertEqual(False, is_empty(BinTree(2,None,None)))
+        self.assertEqual(False, is_empty(BinarySearchTree(2, comes_before, None, None)))
         
     def test_insert(self):
-        self.assertEqual(BinTree(2,None,None),insert(None, BinTree(2,None,None)))
-        self.assertEqual(BinTree(2,None,BinTree(7,None,None)),insert(BinTree(2,None,None),BinTree(7,None,None)))
-        self.assertEqual(BinTree(2, BinTree(-1,None,None),None), insert(BinTree(2,None,None),BinTree(-1,None,None)))
+       self.assertEqual(BinarySearchTree(2,comes_before,None,None), insert(BinarySearchTree(2, comes_before, None, None), 3, comes_before))
+    #     self.assertEqual(BinTree(2,None,BinTree(7,None,None)),insert(BinTree(2,None,None),BinTree(7,None,None)))
+    #     self.assertEqual(BinTree(2, BinTree(-1,None,None),None), insert(BinTree(2,None,None),BinTree(-1,None,None)))
         
-    def test_lookup(self):
-        self.assertEqual(False,lookup(None,4))
-        self.assertEqual(False,lookup(BinTree(3,BinTree(4,None,None),BinTree(-1,None,None)),5))
-        self.assertEqual(True,lookup(BinTree(3,BinTree(4,None,None),BinTree(-1,None,None)),4))
+    # def test_lookup(self):
+    #     self.assertEqual(False,lookup(None,4))
+    #     self.assertEqual(False,lookup(BinTree(3,BinTree(4,None,None),BinTree(-1,None,None)),5))
+    #     self.assertEqual(True,lookup(BinTree(3,BinTree(4,None,None),BinTree(-1,None,None)),4))
         
-    def test_delete(self):
-        self.assertEqual()
+    #def test_delete(self):
+        #self.assertEqual()
         
         
-if (__name__ == '__main__'):
+if __name__ == '__main__':
     unittest.main()
